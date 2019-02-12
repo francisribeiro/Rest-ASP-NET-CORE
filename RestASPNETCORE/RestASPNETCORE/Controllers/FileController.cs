2079b190779c792d0f6f0e324727ba371bc0b973
Francis Ribeiro <francis.silva@verity.com.br>
2019-02-12 17:13:12 -0200

    add docker

--- RestASPNETCORE/RestASPNETCORE/Controllers/FileController.cs
+++ RestASPNETCORE/RestASPNETCORE/Controllers/FileController.cs
@@ -17,7 +17,6 @@ namespace RestASPNETCORE.Controllers
         }
 
         [HttpGet]
-        [AllowAnonymous]
         [ProducesResponseType((200), Type = typeof(byte[]))]
         [ProducesResponseType(204)]
         [ProducesResponseType(400)]
@@ -26,12 +25,14 @@ namespace RestASPNETCORE.Controllers
         public IActionResult GetPDFFile()
         {
             byte[] buffer = _fileBusiness.GetPDFFile();
+
             if (buffer != null)
             {
                 HttpContext.Response.ContentType = "application/pdf";
                 HttpContext.Response.Headers.Add("content-length", buffer.Length.ToString());
                 HttpContext.Response.Body.Write(buffer, 0, buffer.Length);
             }
+
             return new ContentResult();
         }
     }
